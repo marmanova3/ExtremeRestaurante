866720c9d5242b693aee97926cbb678b92228ae8
marmanova3 <marmanova3@uniba.sk>
2019-03-21 15:07:03 +0100

    button action handler

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ExtremeRestaurante
 (čítaj s talianskym prízvukom)
+
+
+https://stackoverflow.com/questions/43198258/javafx-scene-builder-creating-a-new-scene-or-just-making-changes-on-a-specific
--- src/main/java/app/Controller.java
+++ src/main/java/app/Controller.java
@@ -2,10 +2,23 @@
 // Source code recreated from a .class file by IntelliJ IDEA
 // (powered by Fernflower decompiler)
 //
-
 package app;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private Button myButton;
+
+
     public Controller() {
+    }
+
+    @FXML
+    private void handleButtonAction() {
+        // Button was clicked, do something…
+        System.out.println("I was clicked");
     }
 }

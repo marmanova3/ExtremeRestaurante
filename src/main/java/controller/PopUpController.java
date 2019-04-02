23d627cf30df88e191754886e636cdbaecde0c33
Gabriela Slaninkoá <gabika.slani@gmail.com>
2019-04-02 16:00:30 +0200

    popUpController test

--- src/main/java/controller/PopUpController.java
+++ src/main/java/controller/PopUpController.java
@@ -23,14 +23,17 @@ public class PopUpController extends AbstractController {
     private static final String COLOR_RED = "#c90b0b";
 
     private static final String SUCCESS_MESSAGE = "Payment successful";
-    private static final String EROR_MESSAGE = "Payment denied";
+    private static final String ERROR_MESSAGE = "Payment denied";
 
     private static final String DOUBLE_PATTERN = "\\d{0,7}([\\.]\\d{0,2})?";
 
 
-    @FXML private Button confirmBtn, printBtn;
-    @FXML private TextField cashInput;
-    @FXML private Label outlayOutput, message, tableNumber;
+    @FXML
+    private Button confirmBtn, printBtn;
+    @FXML
+    private TextField cashInput;
+    @FXML
+    private Label outlayOutput, message, tableNumber;
     private Stage stage;
     private double priceToPay;
     private List<OrdersEntity> orders;
@@ -43,7 +46,7 @@ public class PopUpController extends AbstractController {
         setCashInputListener();
     }
 
-    private void setCashInputListener(){
+    private void setCashInputListener() {
         cashInput.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
@@ -64,7 +67,7 @@ public class PopUpController extends AbstractController {
         if (receivedCashIsValid(cashInput.getText(), priceToPay)) {
             payOrders();
             showSuccessMessage();
-            outlayOutput.setText(String.format("%.2f", getOutlay(priceToPay, getCashInput())));
+            outlayOutput.setText(getOutlay(priceToPay, getCashInput()));
             confirmBtn.setDisable(true);
             printBtn.setDisable(false);
             cashInput.setDisable(true);
@@ -78,55 +81,63 @@ public class PopUpController extends AbstractController {
     private void printReceipt() {
         String total = Double.toString(priceToPay);
         String cash = Double.toString(getCashInput());
-        String outlay = String.format("%.2f", getOutlay(priceToPay, getCashInput()));
+        String outlay = getOutlay(priceToPay, getCashInput());
         ReceiptPrinter.print(total, cash, outlay, orderItems);
     }
 
-    private Double getCashInput(){
+    private Double getCashInput() {
         return Double.parseDouble(cashInput.getText());
     }
-    private boolean hasDoubleFormat(String value){
+
+    public boolean hasDoubleFormat(String value) {
         return value.matches(DOUBLE_PATTERN);
     }
 
     private void closeStage() {
-        if(this.stage != null) {
+        if (this.stage != null) {
             this.stage.close();
         }
     }
+
     public void setStage(Stage stage) {
         this.stage = stage;
     }
 
-    public void setTableId(int tableId){ this.tableId = tableId; }
-
-    private void setTableInfo(){ tableNumber.setText("TABLE " + (tableId + 1)); }
-
-    public void setPriceToPay(double priceToPay){
-        this.priceToPay = priceToPay;
+    public void setTableId(int tableId) {
+        this.tableId = tableId;
     }
 
-    public void setOrders(List<OrdersEntity> orders){
+    private void setTableInfo() {
+        tableNumber.setText("TABLE " + (tableId + 1));
+    }
+
+    public void setPriceToPay(double priceToPay) {
+        this.priceToPay = priceToPay;
+
+    }
+
+    public void setOrders(List<OrdersEntity> orders) {
         this.orders = orders;
+
     }
 
     public void setOrderItems(List<OrderItemEntity> orderItems) {
         this.orderItems = orderItems;
     }
 
-    private void showSuccessMessage(){
+    private void showSuccessMessage() {
         message.setText(SUCCESS_MESSAGE);
         message.setTextFill(Color.web(COLOR_GREEN));
         message.setVisible(true);
     }
 
-    private void showErrorMessage(){
-        message.setText(EROR_MESSAGE);
+    private void showErrorMessage() {
+        message.setText(ERROR_MESSAGE);
         message.setTextFill(Color.web(COLOR_RED));
         message.setVisible(true);
     }
 
-    private boolean receivedCashIsValid(String cash, Double priceToPay) {
+    public boolean receivedCashIsValid(String cash, Double priceToPay) {
         try {
             Double receivedCashFromCustomer = Double.parseDouble(cash);
             return isGreaterOrEqual(receivedCashFromCustomer, priceToPay);
@@ -135,15 +146,15 @@ public class PopUpController extends AbstractController {
         }
     }
 
-    private boolean isGreaterOrEqual(Double comparingValue, Double desiredValue){
+    private boolean isGreaterOrEqual(Double comparingValue, Double desiredValue) {
         return desiredValue.compareTo(comparingValue) <= 0;
     }
 
-    private Double getOutlay(Double priceToPay, Double receivedCash){
-        return receivedCash - priceToPay;
+    public String getOutlay(Double priceToPay, Double receivedCash) {
+        return String.format("%.2f", receivedCash - priceToPay);
     }
 
-    private void payOrders(){
+    private void payOrders() {
         HibernateQueries.payOrders(orders);
         // TODO update cash_register + priceTotal ... toto este nie je!!!
     }
